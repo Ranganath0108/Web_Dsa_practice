a9557876a70f8f8af32e103403453185b4f613aa
Ranganathaswamy Yelanadu Sunilkumar <rsunilkumar@solifi.com>
2023-01-02 10:24:52 +0530

    initial commit

---
+++ src/main/java/com/example/web_dsa_practice/WebDsaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_dsa_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDsaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDsaPracticeApplication.class, args);
+    }
+
+}
